a6c659380708a4be527ffa0e82d857dc0da88549
hieu <hieuhuule@gmail.com>
2022-10-06 17:12:39 -0500

    added one validation for store_code for models/gym_spec.rb

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -1,4 +1,4 @@
 class Gym < ApplicationRecord
   belongs_to :user
-  validates :gym_name, presence: true
+  validates :gym_name, :store_code, presence: true
 end
--- spec/models/gym_spec.rb
+++ spec/models/gym_spec.rb
@@ -20,4 +20,23 @@ RSpec.describe Gym, type: :model do
     )
     expect(gym.errors[:gym_name]).to_not be_empty
   end
+
+  it "should validate store_code" do
+
+    user = User.create email:"a@a.a" ,  password:"123456", password_confirmation:"123456"
+    gym = user.gyms.create(
+        gym_name: 'Body Tech',
+        hours_of_operations: 'string',
+        phone_number: '875564036',
+        features: "Sauna, Daycare, Yoga",
+        street: '245 bodylane',
+        city: "Pittsburg",
+        state: "PA",
+        email: "BodyTech051@bodytect.com",
+        deal: "25% off supplements!!",
+        deal_code: "BDYT0025",
+        image: "https://bodytech.com.co/uploads/post/291be070af1d4989aef6367e4714c677/piloto-reapertura-pereira-1_1920x854.jpg"
+    )
+    expect(gym.errors[:store_code]).to_not be_empty
+  end
 end
