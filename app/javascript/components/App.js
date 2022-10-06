4d274bc80625fdd64d1c7563f83a1e2d87ce9b9e
hieu <hieuhuule@gmail.com>
2022-10-05 20:27:14 -0500

    added BrowserRouter for index. App not functioning correctly

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,17 +1,20 @@
 import React from "react"
 import Header from "./components/Header"
 import GymIndex from "./pages/GymIndex"
-import { Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { useState, useEffect } from 'react'
 
 const App = (props) => {
+
+
   return (
-    <>
+    <BrowserRouter>
       <Header {...props} />
       <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/gymindex" element={<GymIndex />} />
-    </Routes>
-    </>
+        <Route path="/" element={<Home />} />
+        <Route path="/gymindex" element={<GymIndex />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
