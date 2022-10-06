a424d3ebc949ea314cdf6b9945e4d16e3ba8efc2
hieu <hieuhuule@gmail.com>
2022-10-06 17:14:39 -0500

    added one validation for hours_of_operations for models/gym_spec.rb

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -1,4 +1,4 @@
 class Gym < ApplicationRecord
   belongs_to :user
-  validates :gym_name, :store_code, presence: true
+  validates :gym_name, :store_code, :hours_of_operations, presence: true
 end
--- spec/models/gym_spec.rb
+++ spec/models/gym_spec.rb
@@ -39,4 +39,24 @@ RSpec.describe Gym, type: :model do
     )
     expect(gym.errors[:store_code]).to_not be_empty
   end
+
+  it "should validate hours_of_operations" do
+
+    user = User.create email:"a@a.a" ,  password:"123456", password_confirmation:"123456"
+    gym = user.gyms.create(
+        gym_name: 'Body Tech',
+        store_code: '051',
+        phone_number: '875564036',
+        features: "Sauna, Daycare, Yoga",
+        street: '245 bodylane',
+        city: "Pittsburg",
+        state: "PA",
+        email: "BodyTech051@bodytect.com",
+        deal: "25% off supplements!!",
+        deal_code: "BDYT0025",
+        image: "https://bodytech.com.co/uploads/post/291be070af1d4989aef6367e4714c677/piloto-reapertura-pereira-1_1920x854.jpg"
+    )
+    expect(gym.errors[:hours_of_operations]).to_not be_empty
+  end
+
 end
