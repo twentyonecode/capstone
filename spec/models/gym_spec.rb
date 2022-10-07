68be966b8f18f0d0601d4ed771375563da3c21f5
fpsolis219 <fpsolis21995@gmail.com>
2022-10-06 20:09:10 -0700

    added validation test for deal, deal_code, image

--- spec/models/gym_spec.rb
+++ spec/models/gym_spec.rb
@@ -129,5 +129,88 @@ RSpec.describe Gym, type: :model do
     )  
     expect(gym.errors[:city]).to_not be_empty
   end
+  it "should validate state" do
+    user = User.create email:"a@a.a", password: "123456", password_confirmation:"123456"
+    gym = user.gyms.create(
+      gym_name: "Body Tech",
+      store_code: "051",
+      hours_of_operations: "string",
+      phone_number: "875564036",
+      street: "245 Bodylane",
+      city: "Pittsburg",
+      email: "BodyTech051@bodytect.com",
+      deal: "25% off supplements!!",
+      deal_code: "BDYT0025",
+      image: "https://bodytech.com.co/uploads/post/291be070af1d4989aef6367e4714c677/piloto-reapertura-pereira-1_1920x854.jpg"
+    )  
+    expect(gym.errors[:state]).to_not be_empty
+  end
   
+  it "should validate email" do
+    user = User.create email:"a@a.a", password: "123456", password_confirmation:"123456"
+    gym = user.gyms.create(
+      gym_name: "Body Tech",
+      store_code: "051",
+      hours_of_operations: "string",
+      phone_number: "875564036",
+      street: "245 Bodylane",
+      city: "Pittsburg",
+      state: "PA",
+      deal: "25% off supplements!!",
+      deal_code: "BDYT0025",
+      image: "https://bodytech.com.co/uploads/post/291be070af1d4989aef6367e4714c677/piloto-reapertura-pereira-1_1920x854.jpg"
+    )  
+    expect(gym.errors[:email]).to_not be_empty
+  end
+
+  it "should validate deal" do
+    user = User.create email:"a@a.a", password: "123456", password_confirmation:"123456"
+    gym = user.gyms.create(
+      gym_name: "Body Tech",
+      store_code: "051",
+      hours_of_operations: "string",
+      phone_number: "875564036",
+      street: "245 Bodylane",
+      city: "Pittsburg",
+      state: "PA",
+      email: "BodyTech051@bodytect.com",
+      deal_code: "BDYT0025",
+      image: "https://bodytech.com.co/uploads/post/291be070af1d4989aef6367e4714c677/piloto-reapertura-pereira-1_1920x854.jpg"
+    )  
+    expect(gym.errors[:deal]).to_not be_empty
+  end
+
+  it "should validate deal_code" do
+    user = User.create email:"a@a.a", password: "123456", password_confirmation:"123456"
+    gym = user.gyms.create(
+      gym_name: "Body Tech",
+      store_code: "051",
+      hours_of_operations: "string",
+      phone_number: "875564036",
+      street: "245 Bodylane",
+      city: "Pittsburg",
+      state: "PA",
+      email: "BodyTech051@bodytect.com",
+      deal: "25% off supplements!!",
+      image: "https://bodytech.com.co/uploads/post/291be070af1d4989aef6367e4714c677/piloto-reapertura-pereira-1_1920x854.jpg"
+    )  
+    expect(gym.errors[:deal_code]).to_not be_empty
+  end
+
+  it "should validate image" do
+    user = User.create email:"a@a.a", password: "123456", password_confirmation:"123456"
+    gym = user.gyms.create(
+      gym_name: "Body Tech",
+      store_code: "051",
+      hours_of_operations: "string",
+      phone_number: "875564036",
+      street: "245 Bodylane",
+      city: "Pttsburg",
+      state: "PA",
+      email: "BodyTech051@bodytect.com",
+      deal: "25% off supplements!!",
+      deal_code: "BDYT0025"
+    )  
+    expect(gym.errors[:image]).to_not be_empty
+  end
 end
