9f5bc0a27d16e5deeafc77a17552c0ecd6345e16
TuanLeVietz <tuanz_z@yahoo.com>
2022-10-07 10:48:32 -0700

    added pseudocode for gymController

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -1,12 +1,33 @@
 class GymsController < ApplicationController
+
     def index
         gyms = Gym.all  
         render json: gyms
     end
 
     def create
+        # makes a new instance in the db
+        # check if what is being craeted is valid
+        # return info about new instance in the db
+        # if invalid it returns a 422
+        
     end
 
     def update
+        # updates the instance by :id using params, in the db
+        # check if what is being updated is valid
+        # return info about updated instance in the db
+        # if invalid it returns a 422
     end
+
+    def delete
+        # deletes the instance by :id using params, in the db
+        # check if instance was deleted 
+        # if valid return 204
+        # if invalid return 422
+    end
+        #private section of the class
+        # STRONG PARAMS  
+
 end
+
