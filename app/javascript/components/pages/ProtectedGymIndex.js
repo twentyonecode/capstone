079f982041c618d692efe71dbbb83f4e09fc2f19
fpsolis219 <fpsolis21995@gmail.com>
2022-10-10 10:30:11 -0700

    started work on protected gym index, instructions can be found on the protected gym index. file still in progress

---
+++ app/javascript/components/pages/ProtectedGymIndex.js
@@ -0,0 +1,50 @@
+import React from "react"
+import { Card, CardBody, CardTitle, CardText, Button } from 'reactstrap'
+import { NavLink } from "react-router-dom"
+// need to add this file in the navigation file and also include the features for editing your gyms on this file.
+//also this requires a route path in the app.js
+
+const ProtectedGymIndex = ({ gyms, current_user }) => {
+    const filterGyms = gyms?.filter(gym => gym.user_id === current_user.id)
+    console.log(gyms)
+    // console.log("gyms", gyms)
+    return (
+
+        <main className="gym-index-cards">
+            {gyms?.map((gym, index) => {
+                return (
+                    <Card
+                        color="dark"
+                        outline
+                        style={{
+                            width: '14rem'
+                        }}
+                        key={index}
+                    >
+                        <img
+                            alt=""
+                            src={gym.image}
+                        />
+                        <CardBody>
+                            <div className="gym-name">
+                                <CardTitle>
+                                    {gym.name}
+                                </CardTitle>
+                            </div>
+                            <CardText>Deal: {gym.deals}</CardText>
+                            <Button color="primary" size="lg">
+                                <NavLink to={`/gymshow/${gym.id}`}>
+                                    <div className="button-index-see-more">
+                                        claim your deal
+                                    </div>
+                                </NavLink>
+                            </Button>
+                        </CardBody>
+                    </Card>
+
+                )
+            })}
+        </main>
+    )
+}
+export default ProtectedGymIndex
