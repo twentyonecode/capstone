cf4977ee76720e0b8e6974570c3b262e25e53685
TuanLeVietz <tuanz_z@yahoo.com>
2022-10-10 15:00:57 -0700

    gym create in progress

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -5,7 +5,7 @@ import mockGyms from "./mockGyms"
 import Header from "./components/Header"
 import  Home  from "./pages/Home"
 import NotFound from "./pages/NotFound"
-
+import GymNew from "./pages/GymNew"
 
 const App = (props) => {
 
@@ -23,6 +23,20 @@ const App = (props) => {
   //     .catch((error) => console.log(error))
   // }
 
+  const createGym = (gym) => {
+    console.log("this is a flag", gym)
+    fetch("/gyms", {
+      body: JSON.stringify(gym),
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "POST"
+    })
+    .then(response => response.json())
+    .then(payload => setGyms(payload))
+    .catch(errors => console.log(errors))
+  }
+
   return (
     <BrowserRouter>
       
@@ -31,6 +45,8 @@ const App = (props) => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/gymindex" element={<GymIndex gyms={gyms} />} />
+
+        <Route path="/gymnew" element={<GymNew createGym={createGym} />} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
