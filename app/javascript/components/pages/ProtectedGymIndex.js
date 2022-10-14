2b012d057346d644257db1a30f1660ce031c8874
fpsolis219 <fpsolis21995@gmail.com>
2022-10-14 10:18:10 -0700

    filtergyms

--- app/javascript/components/pages/ProtectedGymIndex.js
+++ app/javascript/components/pages/ProtectedGymIndex.js
@@ -1,55 +1,57 @@
-import React from "react";
-import { Card, CardBody, CardTitle, CardText, Button, CardFooter } from "reactstrap";
-import { NavLink } from "react-router-dom";
-
+import React from "react"
+import { Card, CardBody, CardTitle, CardText, Button } from 'reactstrap'
+import { NavLink } from "react-router-dom"
 // need to add this file in the navigation file and also include the features for editing your gyms on this file.
 //also this requires a route path in the app.js
 
 const ProtectedGymIndex = ({ gyms, current_user }) => {
-  const filterGyms = gyms?.filter((gym) => gym.user_id === current_user.id);
-  return (
-    <>
-      <h1>My gyms</h1>
-      <main className="gym-index-cards">
-        {gyms?.map((gym, index) => {
-          return (
-            <Card
-              className="card-light"
-              color="dark"
-              outline
-              style={{
-                width: "14rem",
-              }}
-              key={index}
-            >
-              <img className="card-light-img" alt="" src={gym.image} />
+    const filterGyms = gyms?.filter(gym => gym.user_id === current_user.id)
+    return (
+        <>
+            <h1>My gyms</h1>
+            <main className="gym-index-cards">
+                {filterGyms?.map((gym, index) => {
+                    return (
+                        <Card
+                            color="dark"
+                            outline
+                            style={{
+                                width: '14rem'
+                            }}
+                            key={index}
+                        >
+                            <img
+                                alt=""
+                                src={gym.image}
+                            />
+                            <CardBody>
+                                <div className="gym-name">
+                                    <CardTitle>
+                                        {gym.gym_name}
+                                    </CardTitle>
+                                </div>
+                                <CardText>Deal: {gym.deal}</CardText>
+                                <Button color="primary" size="lg">
+                                    <NavLink to={`/gymshow/${gym.id}`}>
+                                        <div className="button-index-see-more">
+                                            claim your deal
+                                        </div>
+                                    </NavLink>
+                                </Button>
+                                <Button color="primary" size="lg">
+                                    <NavLink to={`/gymedit/${gym.id}`}>
+                                        <div className="button-index-see-more">
+                                            Update your gym
+                                        </div>
+                                    </NavLink>
+                                </Button>
+                            </CardBody>
+                        </Card>
 
-              <CardBody className="card-body">
-                <div>
-                  <div className="gym-name">
-                    <CardTitle>{gym.gym_name}</CardTitle>
-                  </div>
-                  <CardText className="card-text">Deal: {gym.deal}</CardText>
-                </div>
-                  <Button className="down-button" size="sm">
-                    <NavLink to={`/gymshow/${gym.id}`}>
-                      <div className="button-index-see-more">view your gym</div>
-                    </NavLink>
-                  </Button>
-                  <Button size="sm">
-                    <NavLink to={`/gymedit/${gym.id}`}>
-                      <div className="button-index-see-more">
-                        Update your gym
-                      </div>
-                    </NavLink>
-                  </Button>
-                
-              </CardBody>
-            </Card>
-          );
-        })}
-      </main>
-    </>
-  );
-};
-export default ProtectedGymIndex;
+                    )
+                })}
+            </main>
+        </>
+    )
+}
+export default ProtectedGymIndex
